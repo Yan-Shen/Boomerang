311791649f3bc2c8360ac30b92bb195cec22bf22
aubreyarcangel <aarcangel@gmail.com>
2018-01-24 12:17:42 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import Container from './Container';
+
+
+
+
+
+ReactDOM.render(<Container />, document.getElementById('root'));
+registerServiceWorker();
