18dbed5c806fc8216fa90b7ccb72c20bbf93d665
Yan.Shen <sheny308@yahoo.com>
2018-01-28 10:18:34 -0500

    mergedThunmnail

--- src/modules/slideEdit/containers/SlideEdit.js
+++ src/modules/slideEdit/containers/SlideEdit.js
@@ -37,31 +37,31 @@ class SlideEdit extends Component {
   render() {
     return (
         <div style={{display: 'flex'}}>
-          <div style={{flex: 6.5}}>
-            <Paper style={{margin: "10px"}} zDepth={1}>
-              <CanvasBlock />
-            </Paper>
-           {/* thumbnail area */}
-            <Paper style={{margin: "10px"}} zDepth={1}>
-              <div style={{display: 'flex',height: "140px"}}>
-                {this.state.slides.map(slide => <div style={{margin: '10px', border: '1px solid #ccc'}} key={slide.id}>Slide: {slide.id}</div>)}
-              </div>
-            </Paper>
-          </div>
+        {/* //   <div style={{flex: 6.5}}>
+        //     <Paper style={{margin: "10px"}} zDepth={1}>
+        //       <CanvasBlock />
+        //     </Paper>
+        //    {/* thumbnail area */}
+        {/* //     <Paper style={{margin: "10px"}} zDepth={1}>
+        //       <div style={{display: 'flex',height: "140px"}}>
+        //         {this.state.slides.map(slide => <div style={{margin: '10px', border: '1px solid #ccc'}} key={slide.id}>Slide: {slide.id}</div>)}
+        //       </div>
+        //     </Paper>
+        //   </div> */}
 
-          <div style={{flex: 3.5}}>
-           {/* Toolbox area */}
-            <Paper style={{margin: "10px"}} zDepth={2}>
-            <div className="flex-container-column toolSectionContainer">
-              <ToolContainer />
-              </div>
-            </Paper>
-
-             {/* Display area */}
-            <Paper style={{margin: "10px"}} zDepth={2}>
-              < DisplayContainer />
-            </Paper>
+          {/* <div style={{flex: 3.5}}> */}
+       {/* Toolbox area */}
+        {/* <Paper style={{margin: "10px"}} zDepth={2}>
+        <div className="flex-container-column toolSectionContainer">
+          <ToolContainer />
           </div>
+        </Paper> */}
+
+         {/* Display area */}
+        {/* <Paper style={{margin: "10px"}} zDepth={2}>
+          < DisplayContainer />
+        </Paper>
+      </div>  */}
 
         <SlideEditWrapper {...this.props}/>
       </div>
