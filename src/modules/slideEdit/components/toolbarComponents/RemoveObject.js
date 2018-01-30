ef43beeb0702290ee881462204d8a77a554a3939
Nicholas Vanderpool <nvanderp@MacBook-Pro.attlocal.net>
2018-01-29 22:20:02 -0500

    some fixes

--- src/modules/slideEdit/components/toolbarComponents/RemoveObject.js
+++ src/modules/slideEdit/components/toolbarComponents/RemoveObject.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import {IconMenu, MenuItem,IconButton} from 'material-ui';
-import Delete from 'material-ui/svg-icons/action/delete';
+import React from 'react'
+import {IconMenu, MenuItem,IconButton} from 'material-ui'
+import Delete from 'material-ui/svg-icons/action/delete'
 
 const removeObject = (canvas) => {
-	canvas.remove(canvas.getActiveObject());
+	if (canvas.getActiveObject()._objects) {
+		canvas.getActiveObject()._objects.forEach((object) => {
+			canvas.remove(object)
+		})
+	} else {
+		canvas.remove(canvas.getActiveObject())
+	}
 }
 
 const RemoveObject = ({canvas}) => (
 	<IconButton><Delete onClick={()=>removeObject(canvas)} color="rgba(0,0,0,.3)" /></IconButton>
-);
+)
 
-export default RemoveObject;
+export default RemoveObject
