584bfadd33dfbb61228f8c64ee3ddab47e6e58d6
aubreyarcangel <aarcangel@gmail.com>
2018-02-01 18:09:51 -0500

    styling needed on video embed

--- src/modules/slideEdit/components/overlayComponents/YouTubeDisplay/video_detail.js
+++ src/modules/slideEdit/components/overlayComponents/YouTubeDisplay/video_detail.js
@@ -3,11 +3,10 @@ import React from 'react';
 const VideoDetail = ({url}) => {
 
   return (
-    <div className="video-detail col-md-8">
+
       <div className="embed-responsive embed-responsive-16by9">
         <iframe className="embed-responsive-item" src={url}></iframe>
       </div>
-    </div>
   );
 };
 
